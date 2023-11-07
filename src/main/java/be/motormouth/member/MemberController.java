8134794548ef79ed8ab3c37b749837f80b25430e
ZinebElya <zineb.elyattouti@gmail.com>
2023-11-07 14:23:24 +0100

    add member authorization

--- src/main/java/be/motormouth/member/MemberController.java
+++ src/main/java/be/motormouth/member/MemberController.java
@@ -5,29 +5,39 @@ import be.motormouth.member.dto.MemberDto;
 import be.motormouth.member.entities.MembershipLevel;
 import be.motormouth.member.services.MemberMapper;
 import be.motormouth.member.services.MemberService;
+import be.motormouth.security.SecurityService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.reactive.ResponseStatus;
+import org.jboss.resteasy.reactive.RestHeader;
 
 import java.util.List;
+
+import static be.motormouth.security.Feature.VIEW_ALL_MEMBERS;
+import static be.motormouth.security.Feature.VIEW_A_MEMBER;
 
 @Path("/members")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class MemberController {
-
     @Inject
     MemberService memberService;
 
+    @Inject
+    SecurityService securityService;
+
     @GET
-    public List<MemberDto> getAllMembers(){
+    public List<MemberDto> getAllMembers(@RestHeader String authorization){
+       securityService.validateAuthorization(authorization, VIEW_ALL_MEMBERS);
         return MemberMapper.toDto(memberService.getAllMembers());
     }
 
-    @Path("/{id}")
     @GET
-    public MemberDto getMemberById (@PathParam("id") Long id){
+    @ResponseStatus(200)
+    @Path("/{id}")
+    public MemberDto getMemberById (@RestHeader String authorization, @PathParam("id") Long id){
+        securityService.validateAuthorization(authorization, VIEW_A_MEMBER);
         return MemberMapper.toDto(memberService.getMemberById(id));
     }
 
---
+++ src/test/java/be/motormouth/member/MemberControllerTest.java
@@ -0,0 +1,7 @@
+package be.motormouth.member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberControllerTest {
+
+}
