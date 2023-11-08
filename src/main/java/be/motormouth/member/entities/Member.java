0e07fedbd50486ac58283d3ebe9041ab6973c9bd
Carmen Biesmans <biesmanscarmen@gmail.com>
2023-11-08 14:57:16 +0100

    db relationship fix in Allocation entity, added equals and hashcode in Member

--- src/main/java/be/motormouth/member/entities/Member.java
+++ src/main/java/be/motormouth/member/entities/Member.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "MEMBER")
@@ -86,5 +87,18 @@ public class Member {
 
     public MembershipLevel getMembershipLevel() {
         return membershipLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(Id, member.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
     }
 }
